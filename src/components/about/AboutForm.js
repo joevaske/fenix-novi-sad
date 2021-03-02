43b96f7440838419b3d6934cea9926ee04c6d718
Djordje Vasic <djvasic@domain.local>
2021-03-02 09:05:21 +0100

    changed double id

--- src/components/about/AboutForm.js
+++ src/components/about/AboutForm.js
@@ -7,12 +7,15 @@ const AboutForm = () => {
             <h3>Get a Quote</h3>
             <label className="label" htmlFor="q_name">Full Name</label>
             <input type="text" id="q_name" name="q_name" placeholder="Full name" />
+
             <label className="label" htmlFor="q_email">E mail</label>
             <input type="email" id="q_email" name="q_email" placeholder="Email" />
+
             <label className="label" htmlFor="q_phone">Phone</label>
             <input type="text" id="_phone" name="q_phone" placeholder="Phone" />
+
             <label className="label" htmlFor="q_message">Message</label>
-            <textarea id="q_message" name="q_message" id="q_message" ></textarea>
+            <textarea id="q_message" name="q_message" ></textarea>
             <input type="submit" id="q_submit" value="Send Request" className="btn-form" />
         </form>
     )
